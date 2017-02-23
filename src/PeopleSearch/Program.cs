174087b4cbfdf1a2a211ad6ed5294a761a626c0e
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-02-22 23:26:20 -0500

    Added command line parameters and docker file

--- src/PeopleSearch/Program.cs
+++ src/PeopleSearch/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace PeopleSearch
 {
@@ -15,8 +16,13 @@ namespace PeopleSearch
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(configuration)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
