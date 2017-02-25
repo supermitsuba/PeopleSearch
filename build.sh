f45b76f80d4398880f36f99f1c4c660eee6c8c14
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-02-25 16:44:18 -0500

    Fixed build issue with test projects

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-cd src/PeopleSearch
+
+cd src/PeopleSearch.Data
+dotnet restore && dotnet build **/project.json
+cd ../PeopleSearch.Data.Tests
+dotnet restore && dotnet test ../PeopleSearch.Data.Tests/
+
+cd ../PeopleSearch
 dotnet restore && dotnet build **/project.json
 cd ../PeopleSearch.Tests
 dotnet restore && dotnet test ../PeopleSearch.Tests/ 
