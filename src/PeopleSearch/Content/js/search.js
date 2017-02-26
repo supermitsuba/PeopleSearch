361437439914e15dc638851ad2c833e3375470bd
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-02-26 17:34:32 -0500

    Added code to listen for key presses and delay

--- src/PeopleSearch/Content/js/search.js
+++ src/PeopleSearch/Content/js/search.js
@@ -1,1 +1,19 @@
-alert('hello')
+$(function(){
+    $('#searchPerson').keypress(function(event){
+        var length = this.value.length
+
+        delay(function(){
+            if(length > 1) {
+                console.log(length)
+            }
+        }, 2000)
+    })
+
+    var delay = (function(){
+        var timer = 0;
+        return function(callback, ms){
+            clearTimeout (timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
+})
