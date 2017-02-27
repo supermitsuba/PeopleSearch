36eea9e9face1e1f35addbef379fc7aaf3642423
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-02-26 20:09:19 -0500

    add database migrations to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 cd src/PeopleSearch
 dotnet restore && dotnet build **/project.json
+dotnet ef migrations add MyFirstMigration
+dotnet ef database update
 
 cd ../PeopleSearch.Tests
 dotnet restore && dotnet test ../PeopleSearch.Tests/ 
