8893f21acdc3d47bc1bc403d8c9d092c93393773
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-02-26 21:35:02 -0500

    Going to use hardcoded database instead of building it

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 cd src/PeopleSearch
 dotnet restore && dotnet build **/project.json
-dotnet ef migrations add MyFirstMigration
-dotnet ef database update
 
 cd ../PeopleSearch.Tests
 dotnet restore && dotnet test ../PeopleSearch.Tests/ 
