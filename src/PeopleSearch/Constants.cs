2aa7447ee87413d391814afc04ea2f4f89f644e6
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-02-27 18:19:17 -0500

    Finished commenting code

--- src/PeopleSearch/Constants.cs
+++ src/PeopleSearch/Constants.cs
@@ -1,17 +1,17 @@
 namespace PeopleSearch
 {
     /// <summary>
-    /// 
+    /// A class for all constants
     /// </summary>
     public static class Constants
     {
         /// <summary>
-        /// 
+        /// This is the default for offsets.
         /// </summary>
         public const int DEFAULT_OFFSET = 0;
 
         /// <summary>
-        /// 
+        /// This is the default for the number of records to return.
         /// </summary>
         public const int DEFAULT_LIMIT = 25;
     }    
